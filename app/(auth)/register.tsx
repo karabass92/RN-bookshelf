6cdfd3d33e7e2b8d42f1b9c4aafbc7e5393a1b81
karabass92 <shatveryank@gmail.com>
2025-06-16 21:28:59 +0500

    themed button

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -1,11 +1,18 @@
-import { StyleSheet } from 'react-native'
+import { Pressable, StyleSheet, Text } from 'react-native'
 import { Link } from 'expo-router'
+import { Colors } from '../../constants/colors'
 
 import ThemedView from '../../components/ThemedView'
 import ThemedText from '../../components/ThemedText'
 import Spacer from '../../components/Spacer'
+import ThemedButton from '../../components/ThemedButton'
 
 const Register = () => {
+
+  const handleSubmit = async () => {
+    console.log('register form submitted')
+  }
+
   return (
     <ThemedView style={styles.container}>
 
@@ -14,8 +21,19 @@ const Register = () => {
         Register an Account
       </ThemedText>
 
+      {/* <Pressable
+        onPress={handleSubmit}
+        style={({ pressed }) => [styles.btn, pressed && styles.pressed]}
+      >
+        <Text style={{color: "#f2f2f2"}}>Register</Text>
+      </Pressable> */}
+
+      <ThemedButton onPress={handleSubmit}>
+        <Text style={{ color: '#f2f2f2' }}>Register</Text>
+      </ThemedButton>
+
       <Spacer height={100} />
-      <Link href="/bookshelf/app/(auth)/login" replace>
+      <Link href="/login" replace>
         <ThemedText style={{ textAlign: "center" }}>
           Login instead
         </ThemedText>
@@ -31,10 +49,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: "center",
+    alignItems: "center"
   },
   title: {
     textAlign: "center",
     fontSize: 18,
     marginBottom: 30
   },
+  btn: {
+    backgroundColor: Colors.primary,
+    padding: 15,
+    borderRadius: 5,
+  },
+  pressed: {
+    opacity: 0.8
+  }
 })
