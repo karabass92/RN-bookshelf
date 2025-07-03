2ecfc8248814d2df613ece30560a6007c07333c0
karabass92 <shatveryank@gmail.com>
2025-07-03 09:19:06 +0500

    create new records

--- contexts/BooksContext.tsx
+++ contexts/BooksContext.tsx
@@ -1,12 +1,16 @@
 import { createContext, useState } from "react"
+import { databases } from "../lib/appwrite"
+import { ID, Permission, Role } from "react-native-appwrite"
+import { useUser } from "../hooks/useUser"
 
-const DATABASE_ID = "67b1c7de000c3e31c359"
-const COLLECTION_ID = "67b1c7e500143ab954ca"
+const DATABASE_ID = "67cdd0c9002307459723"
+const COLLECTION_ID = "67cdd0cf00307157221e"
 
 export const BooksContext = createContext()
 
 export function BooksProvider({children}) {
   const [books, setBooks] = useState([])
+  const { user } = useUser()
 
   async function fetchBooks() {
     try {
@@ -19,8 +23,6 @@ export function BooksProvider({children}) {
   async function fetchBookById(id) {
     try {
 
-  
-      return response 
     } catch (error) {
       console.log(error.message)
     }
@@ -28,7 +30,17 @@ export function BooksProvider({children}) {
 
   async function createBook(data) {
     try {
-      
+      await databases.createDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        ID.unique(),
+        {...data, userId: user.$id},
+        [
+          Permission.read(Role.user(user.$id)),
+          Permission.update(Role.user(user.$id)),
+          Permission.delete(Role.user(user.$id)),
+        ]
+      )
     } catch (error) {
       console.log(error.message)
     }
@@ -36,14 +48,14 @@ export function BooksProvider({children}) {
 
   async function deleteBook(id) {
     try {
-      
+
     } catch (error) {
       console.log(error.message)
     }
   }
 
   return (
-    <BooksContext.Provider 
+    <BooksContext.Provider
       value={{ books, fetchBooks, fetchBookById, createBook, deleteBook }}
     >
       {children}
