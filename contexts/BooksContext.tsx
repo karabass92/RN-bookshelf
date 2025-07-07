33867d3ee829ea4377b60c78b3e270e6db38517e
karabass92 <shatveryank@gmail.com>
2025-07-07 16:00:08 +0500

    fetch single records

--- contexts/BooksContext.tsx
+++ contexts/BooksContext.tsx
@@ -31,7 +31,13 @@ export function BooksProvider({children}) {
 
   async function fetchBookById(id) {
     try {
+      const response = await databases.getDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        id
+      )
 
+      return response
     } catch (error) {
       console.log(error.message)
     }
